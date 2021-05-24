ce88748f3602db8b6d3e343ec499c962d8eb7e73
ktritsh7 <ktriesh7@gmail.com>
2021-05-24 08:43:54 +0530

    Welcom to Hotel Reservation System

---
+++ HotelReservationSystem.java
@@ -0,0 +1,10 @@
+package com.bridgelabz.hotelreservationsystem;
+
+public class HotelReservationSystem {
+
+	public static void main(String[] args) {
+		System.out.println("Welcome to Hotel Reservation System");
+
+	}
+
+}
